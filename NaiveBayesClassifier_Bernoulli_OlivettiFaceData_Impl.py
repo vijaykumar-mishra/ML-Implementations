8aa9ac63ae57a072e958e16c865e7688245234bb
vijaykumar-mishra <vijay.someonespecial@gmail.com>
2024-10-14 16:04:29 +0530

    Bernulli Naive Bayes Classifier applied over Olivetti Faces data
    Code and Description doc for Bernulli Naive Bayes Classifier applied over Olivetti Faces data

---
+++ NaiveBayesClassifier_Bernoulli_OlivettiFaceData_Impl.py
@@ -0,0 +1,68 @@
+# Bernoulli Naive Bayes classifier
+# Download the Olivetti faces dataset. 
+# Visit https://scikit-learn.org/0.19/datasets/olivetti_faces.html
+# There are 40 classes (corresponding to 40 people), each class having 10 faces of the individual; so there are a total of 400 images. 
+# Here each face is viewed as an imgae of size 64 × 64 (= 4096) pixels; each pixel having values 0 to 255 which are ultimately converted into floating numbers in the range [0,1]. 
+# Split the dataset into train and test parts such that there are 320 images, 8 images per person (8 X 40 ) for training and 80 images, 2 images per person, (2 X 40) for testing.
+# Repeat the experiment using 10 diferent random splits having 320 training faces and 80 test faces as specified above and report the average accuracy.
+# Convert the data into binary form by replacing any feature value below 0.5 by a 0 and value greater than or equal to 0.5 by a 1. 
+# Use the Bernoulli Naive Bayes classifier to classify the test data and report the results
+
+
+import numpy as np
+import math
+import random
+import pandas as pd
+from sklearn.datasets import fetch_olivetti_faces
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
+
+dataset = fetch_olivetti_faces()
+
+target = dataset.target  #numpy array of shape (400, )
+faces = dataset.images   #numpy array of shape (400, 64, 64)
+X =  dataset.data #numpy array of share (400,4096)
+
+X_train_arr = []
+X_test_arr = []
+y_train_arr = []
+y_test_arr = []
+
+total_accuracy = 0
+
+for row in range(400):
+    for col in range(4096):
+        if(X[row,col]<0.5):
+            X[row,col] = 0
+        else:
+            X[row,col] = 1
+        
+
+for n in range(10):
+    for i in range(40):
+        X_inp = []
+        target_inp = []
+        for j in range(10):
+            ind = i*10+j
+            X_inp.append(X[ind,:])
+            target_inp.append(target[ind])
+
+        X_train, X_test, y_train, y_test = train_test_split(X_inp, target_inp, test_size=0.2,stratify=target_inp)
+        X_train_arr.extend(X_train)
+        X_test_arr.extend(X_test)
+        y_train_arr.extend(y_train)
+        y_test_arr.extend(y_test)
+
+    model = BernoulliNB()
+    model.fit(X_train_arr,y_train_arr)
+
+    pred_i_test = model.predict(X_test_arr)
+    accuracy = accuracy_score(y_test_arr,pred_i_test)
+    total_accuracy = total_accuracy+accuracy
+    
+    print("accuracy_score for iteration", n+1,  accuracy)
+    
+print("Avearge accuracy  = ", total_accuracy/10)
+
